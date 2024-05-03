c117c41074268d55ff900bdf8fe685e4ca739ca0
plotterartist <plotterartist@theplotnft.com>
2024-05-03 21:42:44 +0200

    Update websocket_server.bat

--- files/websocket_server.bat
+++ files/websocket_server.bat
@@ -1,3 +1,3 @@
 @echo off
-python.exe "C:/Users/theplotnft/Python/theplotnft-ws-server/theplotnft_ws_server/ws_server.py" C:/Users/theplotnft/Python/settings/ws_server C:/Users/theplotnft/Python/settings/obs
+python.exe "C:/Users/theplotnft/Python/theplotnft-ws-server/theplotnft_ws_server/ws_server.py" C:/Users/theplotnft/Python/settings/ws_server C:/Users/theplotnft/Python/settings/obs INFO
 pause
